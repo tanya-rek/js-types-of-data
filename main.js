785f005d45d71e87d322bb73a6bdb274b9e9c64b
tanya-rek <rekun.tetiana@gmail.com>
2024-04-02 00:40:02 -0400

    Add new project with scripts

---
+++ main.js
@@ -0,0 +1,49 @@
+// 1
+// const yourName = prompt("What is your name?");
+// console.log(`Hello, ${yourName}`);
+
+//2
+// let firstChildName = prompt("Please type your child's name");
+// let firstChildAge = prompt("Please type your child's age");
+
+// let secondChildName = prompt("Please type your child's name");
+// let secondChildAge = prompt("Please type your child's age");
+
+// console.log(firstChildAge > secondChildAge && firstChildName === "Tim");
+
+//3
+// let yourColor = prompt("Please type a color");
+
+// console.log(
+//   yourColor === "white" ||
+//     yourColor === "black" ||
+//     yourColor === "yellow" ||
+//     yourColor === "green" ||
+//     yourColor === "red" ||
+//     yourColor === "blue"
+// );
+
+//4
+// let yourRandomNumber = prompt("Please write a number");
+// let result = yourRandomNumber % 2;
+// console.log(result != 0);
+
+//5
+// let yourFirstNumber = prompt("Please write your 1st number");
+// let yourSecondNumber = prompt("Please write your 2nd number");
+// let result = yourFirstNumber / yourSecondNumber;
+// console.log(parseInt(result));
+
+//6
+// const yearNow = 2024;
+// let yourBirthYear = prompt("What is your year of birth?");
+// let yourAge = yearNow - yourBirthYear;
+// console.log(yourAge > 18);
+
+//7
+// let randomVariable;
+// console.log(typeof randomVariable);
+
+//8
+// let yourSomeNumber = prompt("Please write a number");
+// console.log(yourSomeNumber > 10 && yourSomeNumber < 50);
